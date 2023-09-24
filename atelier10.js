891743db68a60b4c10042e21e491cc520f64f329
Felix Parent <parent.felix@gmail.com>
2023-09-24 12:19:34 -0400

    Ajout de socket io et enlever la page recherche

--- atelier10.js
+++ atelier10.js
@@ -6,6 +6,9 @@ var express = require('express');
 var app = express();
 app.set('view engine', 'ejs');
 
+var server = app.listen(8080);  
+var io = require('socket.io')(server);
+
 app.get('/',function(req,res){   
     res.status(200).render('./pages/accueil');
 });
@@ -18,5 +21,8 @@ app.get('/recherche/:annee/:mois',function(req,res,next){
 app.use(function(req,res,next){
     res.status(404).end('ERREUR 404!');   
 });
-app.listen(8080);
+// Quand un client se connecte, on le note dans la console
+io.sockets.on('connection', function (socket) {
+    console.log('Un client est connecté !');
+});
 console.log("Le serveur est lancé sur le port 8080");
