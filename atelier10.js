89d6ad1a3b0719600386ce8e4dbb2e048fc49840
Felix Parent <parent.felix@gmail.com>
2023-09-25 11:01:43 -0400

    Ajout d'une alarme, modif de la navbar

--- atelier10.js
+++ atelier10.js
@@ -33,7 +33,7 @@ var heure   = now.getHours();
 var minute  = now.getMinutes();
 var seconde = now.getSeconds();
 
-var dateServeur = "Le serveur a été lancé le " + jour + "/" + mois + "/" + annee + " à " + heure + ":" + minute + ":" + seconde;
+var dateServeur = "Le serveur a été lancé le " + jour + "/" + mois + "/" + annee + " à " + heure + ":" + add_zero(minute,2) + ":" + add_zero(seconde,2);
 
 io.sockets.on('connection', function (socket) {
     
@@ -44,12 +44,26 @@ io.sockets.on('connection', function (socket) {
     var heure   = now.getHours();
     var minute  = now.getMinutes();
     var seconde = now.getSeconds();
-    var dateRefresh = "Une nouvelle demande a été faite le " + jour + "/" + mois + "/" + annee + " à " + heure + ":" + minute + ":" + seconde;
+    var dateRefresh = "Une nouvelle demande a été faite le " + jour + "/" + mois + "/" + annee + " à " + heure + ":" + add_zero(minute,2) + ":" + add_zero(seconde,2);
 
     socket.emit('message1', 'Vous êtes le ' + noClient + "e client") ;
     socket.emit('message2', dateServeur);
     socket.emit('message3', dateRefresh);
     console.log('Un client est connecté !');
     noClient++;
+
+    socket.on('message4', function (message) {
+        console.log('Un client me parle ! Il me dit : ' + message);
+        socket.broadcast.emit('messageBroadcast', message);
+    });
 });
+
 console.log("Le serveur est lancé sur le port 8080");
+
+function add_zero(your_number, length) {
+    var num = '' + your_number;
+    while (num.length < length) {
+        num = '0' + num;
+    }
+    return num;
+}   
